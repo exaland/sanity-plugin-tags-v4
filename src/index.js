c539540103084c2a08989344ba40a2db852a79e1
Alexandre Magnier <exaland@gmail.com>
2026-06-27 23:28:19 +0200

    fix: [plugin:vite:import-analysis] Failed to resolve entry for package "sanity-plugin-tags-v4"

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import { definePlugin } from 'sanity';
-import { tagSchema } from './schemas/tag';
-import { tagsSchema } from './schemas/tags';
-export const tags = definePlugin(() => ({
-    name: 'sanity-plugin-tags',
-    schema: {
-        types: [tagSchema, tagsSchema],
-    },
-}));
